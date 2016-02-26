5705c8313d55938f152f3304c451d1584d62f7bf
rogilmore <thatrickgilmore@gmail.com>
2016-02-26 15:26:27 -0500

    Import images from Databrary

---
+++ analysis/shade-etal-2016/load.wp.R
@@ -0,0 +1,18 @@
+load.wp <- function(g, e, fn.df='../csv/wallpapers-on-databrary.csv'){
+  # Loads selected wallpaper image from Databrary
+  
+  require(dplyr)
+  # Load data frame of wallpaper identifiers, filter and select
+  wp.db <- read.csv(fn.df, header=TRUE)
+  s <- wp.db %>% filter(group==g)
+  slot <- s[e,'slot']
+  asset <- s[e,'asset']
+  
+  # Build URL and download
+  url <- paste('https://nyu.databrary.org/slot', slot, '-/asset', asset, 'download?inline=true', sep='/')
+  z = tempfile()
+  download.file(url, z)
+  p <- readPNG(z)
+  file.remove(z)
+  p
+}
